ff84f90e5a347696230e04253fee577850183b71
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2021-05-18 08:31:47 -0500

    Add more purchase options (#6)
    * Add Amazon links options (non wired in yet)
    
    * Wire up Amazon links, style options popup
    
    * Minor refactor to content script
    
    * #2 add eBooks.com search links
    
    * Add updated screenshots

---
+++ src/book-pane-links.js
@@ -0,0 +1,138 @@
+{
+    const DEFAULT_LIBRARY_NAME = 'lincoln';
+
+    const BOOK_PANE_SELECTOR = '.book-pane[data-book-id]';
+
+    const LIBBY_LINK_SELECTOR = 'libby-link';
+    const LIBBY_LINK_CLASSES = `${LIBBY_LINK_SELECTOR} text-xs cursor-pointer hover:text-cyan-700 border-b`;
+    const LIBBY_LINK_TEXT = 'Library';
+
+    const BUY_LINK_CLASSES = 'font-semibold hover:text-cyan-700';
+    const DEFAULT_AMAZON_DOMAIN = '.com';
+    const AMAZON_LINK_SELECTOR = 'amazon-link';
+    const BUY_LINK_SELECTOR = 'buy-book-pane';
+    const AMAZON_LINK_CLASSES = `${AMAZON_LINK_SELECTOR} ${BUY_LINK_CLASSES}`;
+    const amazonLinkText = (amazonDomain) => `Amazon${amazonDomain}`;
+
+    const EBOOKS_LINK_SELECTOR = 'ebooks-link';
+    const EBOOKS_LINK_CLASSES = `${EBOOKS_LINK_SELECTOR} ${BUY_LINK_CLASSES}`;
+    const EBOOKS_LINK_TEXT = 'eBooks.com';
+
+    const buildLibbyLinkUrl = (libraryName, title, author) => encodeURI(`https://libbyapp.com/search/${libraryName}/search/query-${title} ${author}/page-1`);
+    const buildAmazonLinkUrl = (amazonDomain, title, author) => encodeURI(`https://amazon${amazonDomain}/s?k=${title}+${author}`);
+    const buildEbooksLinkUrl = (title, author) => encodeURI(`https://www.ebooks.com/searchapp/searchresults.net?term=${title}+${author}`);
+
+    const buildLinkElement = ({ url, text, classNames }) => {
+        const linkElement = document.createElement('a');
+        linkElement.className = classNames;
+        linkElement.href = url;
+        linkElement.textContent = text;
+        linkElement.target = '_blank';
+        return linkElement;
+    };
+
+    const getBookData = (bookPane) => {
+        const title = bookPane.getElementsByTagName('h3')[0].innerText;
+        const author = bookPane.getElementsByClassName('mb-1 text-xs')[0].innerText;
+        return {
+            title: title,
+            author: author,
+        };
+    }
+
+    const appendLibraryLink = (bookPane, libraryName, { title, author }) => {
+        if (bookPane.getElementsByClassName(LIBBY_LINK_SELECTOR).length) return;
+
+        const actionLinks = bookPane.getElementsByClassName('book-action-links')[0];
+        const libbyLink = buildLinkElement({
+            url: buildLibbyLinkUrl(libraryName, title, author),
+            text: LIBBY_LINK_TEXT,
+            classNames: LIBBY_LINK_CLASSES,
+        });
+        actionLinks.append(libbyLink);
+    };
+
+    const isUsAmazonDomain = (amazonDomain) => amazonDomain === DEFAULT_AMAZON_DOMAIN || amazonDomain === '.us';
+
+    const appendAmazonLink = (bookPane, selectedAmazonDomain, { title, author}) => {
+        if (bookPane.getElementsByClassName(AMAZON_LINK_SELECTOR).length) return;
+
+        const buyLinks = bookPane.getElementsByClassName(BUY_LINK_SELECTOR)[0].getElementsByTagName('p')[isUsAmazonDomain(selectedAmazonDomain) ? 0 : 1];
+
+        const amazonLinkElement = buildLinkElement({
+            url: buildAmazonLinkUrl(selectedAmazonDomain, title, author),
+            text: amazonLinkText(selectedAmazonDomain),
+            classNames: AMAZON_LINK_CLASSES,
+        });
+        buyLinks.append(document.createElement('br'), amazonLinkElement);
+    };
+
+    const appendEbooksLinks = (bookPane, { title, author }) => {
+        if (bookPane.getElementsByClassName(EBOOKS_LINK_SELECTOR).length) return;
+        
+        const buyLinks = [...bookPane.getElementsByClassName(BUY_LINK_SELECTOR)[0].getElementsByTagName('p')].slice(0, 2);
+        
+        // append links to both `United States` and `Other countries` category and let eBooks.com handle locale
+        const ebooksLinkElement = buildLinkElement({
+            url: buildEbooksLinkUrl(title, author),
+            text: EBOOKS_LINK_TEXT,
+            classNames: EBOOKS_LINK_CLASSES,
+        });
+        buyLinks.forEach(link => link.append(document.createElement('br'), ebooksLinkElement.cloneNode(true)));
+    };
+
+    const buildAllLinks = (bookPanes, {
+        librarySettings: { libraryLinksEnabled, libraryName },
+        amazonSearchSettings: { amazonSearchLinksEnabled, selectedAmazonDomain },
+        ebooksSearchSettings: { ebooksSearchLinksEnabled },
+    }) => {
+        if (!libraryLinksEnabled) removeLinks(LIBBY_LINK_SELECTOR);
+        if (!amazonSearchLinksEnabled) removeLinks(AMAZON_LINK_SELECTOR);
+        if (!ebooksSearchLinksEnabled) removeLinks(EBOOKS_LINK_SELECTOR);
+
+        if (!libraryLinksEnabled &&
+            !amazonSearchLinksEnabled &&
+            !ebooksSearchLinksEnabled) return;
+
+        [...bookPanes].forEach(bookPane => {
+            const bookData = getBookData(bookPane);
+            if (libraryLinksEnabled) appendLibraryLink(bookPane, libraryName, bookData);
+            if (amazonSearchLinksEnabled) appendAmazonLink(bookPane, selectedAmazonDomain, bookData);
+            if (ebooksSearchLinksEnabled) appendEbooksLinks(bookPane, bookData);
+        });
+    };
+
+    const getAllBookPanes = () => document.querySelectorAll(BOOK_PANE_SELECTOR);
+
+    const removeLinks = (selector) => {
+        const allLinks = document.getElementsByClassName(selector);
+        [...allLinks].forEach(link => link.remove());
+    };
+
+    const insertLinks = () => {
+        chrome.storage.sync.get({
+            librarySettings: {
+                libraryLinksEnabled: true,
+                libraryName: DEFAULT_LIBRARY_NAME,
+            },
+            amazonSearchSettings: {
+                amazonSearchLinksEnabled: true,
+                selectedAmazonDomain: DEFAULT_AMAZON_DOMAIN,
+            },
+            ebooksSearchSettings: { ebooksSearchLinksEnabled: true, },
+        }, (userSettings) => {
+            const bookPanes = getAllBookPanes();
+            buildAllLinks(bookPanes, userSettings);
+        });
+    };
+
+    const observeMainContainer = () => {
+        const mainContainerElements = document.getElementsByTagName('html')[0];
+        const observerConfig = { attributes: false, childList: true, subtree: true };
+        const observer = new MutationObserver(insertLinks);
+        observer.observe(mainContainerElements, observerConfig);
+    };
+
+    insertLinks();
+    observeMainContainer();
+}
--- src/options/options.js
+++ src/options/options.js
@@ -1,11 +1,122 @@
 const DEFAULT_LIBRARY_NAME = 'lincoln';
+
 const LIBRARY_NAME_INPUT_SELECTOR = 'library-name';
 const STATUS_TEXT_SELECTOR = 'status';
+const LIBRARY_LINKS_ENABLED_SELECTOR = 'enable-library-links';
+const AMAZON_LINKS_ENABLED_SELECTOR = 'enable-amazon-search';
+const AMAZON_DOMAIN_SELECTOR = 'amazon-domain';
+
+const EBOOKS_LINKS_ENABLED_SELECTOR = 'enable-ebooks-search';
+
+const BASE_AMAZON_DOMAIN = 'https://amazon';
+const DEFAULT_AMAZON_DOMAIN = '.com';
+const amazonDomains = [
+    {
+        groupName: 'Africa',
+        options: [
+            { value: '.ac', name: 'Ascension Island' },
+            { value: '.co.za', name: 'South Africa' },
+            { value: '.sh', name: 'Saint Helena' },
+        ],
+    },
+    {
+        groupName: 'Americas',
+        options: [
+            { value: '.ca', name: 'Canada' },
+            { value: '.cl', name: 'Chile' },
+            { value: '.co', name: 'Columbia' },
+            { value: '.com.ar', name: 'Argentina' },
+            { value: '.com.br', name: 'Brazil' },
+            { value: '.mx', name: 'Mexico' },
+            { value: '.us', name: 'United States' },
+            { value: '.vc', name: 'Saint Vincent and the Grenadines' },
+            { value: '.vg', name: 'British Virgin Islands' },
+        ]
+    },
+    {
+        groupName: 'Asia/Oceania',
+        options: [
+            { value: '.cc', name: 'Cocos (Keeling) Islands' },
+            { value: '.co.nz', name: 'New Zealand' },
+            { value: '.com.au', name: 'Australia' },
+            { value: '.com.sg', name: 'Republic of Singapore' },
+            { value: '.fm', name: 'Federated States of Micronesia' },
+            { value: '.in', name: 'India' },
+            { value: '.jp', name: 'Japan' },
+            { value: '.io', name: 'British Indian Ocean Territory' },
+            { value: '.qa', name: 'Qatar' },
+            { value: '.ru', name: 'Russian Federation' },
+        ]
+    },
+    {
+        groupName: 'Europe',
+        options: [
+            { value: '.be', name: 'Belgium' },
+            { value: '.berlin', name: 'City of Berlin in Germany' },
+            { value: '.ch', name: 'Switzerland' },
+            { value: '.co.uk', name: 'United Kingdom' },
+            { value: '.cz', name: 'Czech Republic' },
+            { value: '.de', name: 'Germany' },
+            { value: '.es', name: 'Spain' },
+            { value: '.eu', name: 'European Union' },
+            { value: '.fi', name: 'Finland' },
+            { value: '.fr', name: 'France' },
+            { value: '.gg', name: 'Guernsey' },
+            { value: '.im', name: 'Isle of Man' },
+            { value: '.it', name: 'Italy' },
+            { value: '.me', name: 'Montenegro' },
+            { value: '.nl', name: 'The Netherlands' },
+            { value: '.ruhr', name: 'Ruhr region, western part of Germany' },
+            { value: '.sw', name: 'Sweden' },
+            { value: '.wien', name: 'City of Vienna in Austria' },
+        ]
+    },
+];
+
+const toggleLibraryLinks = ({ target: { checked }}) => toggleInputDisabledAttribute(!checked, LIBRARY_NAME_INPUT_SELECTOR);
+
+const toggleAmazonLinks = ({ target: { checked }}) => toggleInputDisabledAttribute(!checked, AMAZON_DOMAIN_SELECTOR);
+
+const toggleInputDisabledAttribute = (disabled, selector) => {
+    const input = document.getElementById(selector);
+    if (disabled) {
+        input.setAttribute('disabled', 'true');
+    } else {
+        input.removeAttribute('disabled');
+    }
+};
+
+const buildAmazonOptions = (selectedDomain) => {
+    const defaultOption = new Option('Amazon.com', DEFAULT_AMAZON_DOMAIN, true, selectedDomain === DEFAULT_AMAZON_DOMAIN);
+    const groups = amazonDomains.map(({ groupName, options }) => {
+        const optGroup = document.createElement('optgroup');
+        optGroup.label = groupName;
+        const subOptions = options.map(({ value, name }) => new Option(name, value, false, value === selectedDomain));
+        optGroup.append(...subOptions);
+        return optGroup;
+    });
+    return [defaultOption, ...groups];
+};
 
 const saveOptions = () => {
-    var libraryName = document.getElementById(LIBRARY_NAME_INPUT_SELECTOR).value;
+    const libraryLinksEnabled = document.getElementById(LIBRARY_LINKS_ENABLED_SELECTOR).checked;
+    const libraryName = document.getElementById(LIBRARY_NAME_INPUT_SELECTOR).value;
+
+    const amazonSearchLinksEnabled = document.getElementById(AMAZON_LINKS_ENABLED_SELECTOR).checked;
+    const selectedAmazonDomain = document.getElementById(AMAZON_DOMAIN_SELECTOR).value || DEFAULT_AMAZON_DOMAIN;
+
+    const ebooksSearchLinksEnabled = document.getElementById(EBOOKS_LINKS_ENABLED_SELECTOR).checked;
+
     chrome.storage.sync.set({
-        libraryName: libraryName,
+        librarySettings: {
+            libraryLinksEnabled: libraryLinksEnabled,
+            libraryName: libraryName,
+        },
+        amazonSearchSettings: {
+            amazonSearchLinksEnabled: amazonSearchLinksEnabled,
+            selectedAmazonDomain: selectedAmazonDomain,
+        },
+        ebooksSearchSettings: { ebooksSearchLinksEnabled: ebooksSearchLinksEnabled, },
     }, () => {
         var status = document.getElementById(STATUS_TEXT_SELECTOR);
         status.textContent = 'Options saved.';
@@ -17,11 +128,38 @@ const saveOptions = () => {
 
 const restoreOptions = () => {
     chrome.storage.sync.get({
-        libraryName: DEFAULT_LIBRARY_NAME,
-    }, ({ libraryName }) => {
-        document.getElementById(LIBRARY_NAME_INPUT_SELECTOR).value = libraryName;
+        librarySettings: {
+            libraryLinksEnabled: true,
+            libraryName: DEFAULT_LIBRARY_NAME,
+        },
+        amazonSearchSettings: {
+            amazonSearchLinksEnabled: true,
+            selectedAmazonDomain: DEFAULT_AMAZON_DOMAIN,
+        },
+        ebooksSearchSettings: { ebooksSearchLinksEnabled: true, },
+    }, ({
+            librarySettings: { libraryLinksEnabled, libraryName },
+            amazonSearchSettings: { amazonSearchLinksEnabled, selectedAmazonDomain },
+            ebooksSearchSettings: { ebooksSearchLinksEnabled },
+        }) => {
+        document.getElementById(LIBRARY_LINKS_ENABLED_SELECTOR).checked = libraryLinksEnabled;
+        const libraryNameInput = document.getElementById(LIBRARY_NAME_INPUT_SELECTOR);
+        libraryNameInput.value = libraryName;
+        libraryNameInput.disabled = !libraryLinksEnabled;
+        
+        document.getElementById(AMAZON_LINKS_ENABLED_SELECTOR).checked = amazonSearchLinksEnabled;
+
+        const amazonOptions = buildAmazonOptions(selectedAmazonDomain);
+        const amazonSelect = document.getElementById(AMAZON_DOMAIN_SELECTOR);
+        amazonSelect.append(...amazonOptions);
+        amazonSelect.disabled = !amazonSearchLinksEnabled;
+
+        document.getElementById(EBOOKS_LINKS_ENABLED_SELECTOR).checked = ebooksSearchLinksEnabled;
     });
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
+
+document.getElementById(LIBRARY_LINKS_ENABLED_SELECTOR).addEventListener('change', toggleLibraryLinks);
+document.getElementById(AMAZON_LINKS_ENABLED_SELECTOR).addEventListener('change', toggleAmazonLinks);
