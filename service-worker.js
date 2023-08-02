1bf8972f506772e9d22542f723af460240648d93
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-08-02 08:35:09 -0500

    Right click to search (#18)
    * #17 Add right click to search context menu
    
    * Update changelog-check action

--- service-worker.js
+++ service-worker.js
@@ -17,6 +17,17 @@ const openNewTab = (url) => {
     });
 };
 
+const searchContextMenuItemId = 'storygraph-search';
+const searchContextMenuCreateProperties = {
+    contexts: ['selection'],
+    enabled: true,
+    id: searchContextMenuItemId,
+    title: 'Search The StoryGraph',
+    visible: true,
+};
+
+const storygraphSearchUrl = (searchTerm) => `https://app.thestorygraph.com/browse?search_term=${searchTerm}`;
+
 const handleOnInstalledEvent = ({ reason, previousVersion = '' }) => {
     const previousMajorVersion = parseInt(previousVersion.split('.')[0]);
     const urlBuilder = INSTALL_REASON_URLS[reason];
@@ -24,6 +35,20 @@ const handleOnInstalledEvent = ({ reason, previousVersion = '' }) => {
 
     const url = urlBuilder(previousMajorVersion);
     if (url) openNewTab(url);
+
+    chrome.contextMenus.create(searchContextMenuCreateProperties);
 };
 
 chrome.runtime.onInstalled.addListener(handleOnInstalledEvent);
+
+chrome.contextMenus.onClicked.addListener(
+    (info) => {
+        const { menuItemId, selectionText } = info;
+        switch (menuItemId) {
+            case searchContextMenuItemId:
+                if (!selectionText) return;
+                openNewTab(storygraphSearchUrl(selectionText));
+                break;
+        }
+    }
+);
